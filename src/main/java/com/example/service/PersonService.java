7374e67f60c26b6374eb374120364bd545c18f21
sayed mahmoud <sayedscu@gmail.com>
2022-06-05 11:45:57 +0200

    Update PersonService.java
    p = new Person();
    		p.setId("8");
    		p.setAge(33);
    		p.setFirstName("sayed");
    		p.setLastName("Mahmoud");
    		persons.add(p);

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -62,6 +62,13 @@ public class PersonService {
 		p.setFirstName("sayed");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+		
+		p = new Person();
+		p.setId("8");
+		p.setAge(33);
+		p.setFirstName("sayed");
+		p.setLastName("Mahmoud");
+		persons.add(p);
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
